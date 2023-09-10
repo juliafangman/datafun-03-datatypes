f6650d0ae1629efdd4aa6289d8ad3dd831f9cad4
Julia Fangman <S567521@nwmissouri.edu>
2023-09-10 12:10:11 -0500

    Bonus

--- xtra_p3.py
+++ xtra_p3.py
@@ -41,36 +41,21 @@ def compare_two_plays():
 
     logger.info(f"List of words from play 2: {wordlist2}")
 
-
     # Done with files - let the files close and the work begin
-
-    # Remove duplicates by creating two sorted sets
-    # hint: use sorted() to sort the list
-    # hint: use set() to remove duplicates
-    # name them wordset1 and wordset2
-    wordset1 = set()  # TODO fix this line
-    wordset2 = set()  # TODO fix this line
+    
+    wordset1 = set(sorted(wordlist1))  
+    wordset2 = set(sorted(wordlist2))  
 
 
     # initialize a variable maxlen = 10
-    maxlen = 1  # TODO fix this line
+    maxlen = 10  
+    
+    pre_longwordset1 = [word for word in wordset1 if len(word) > maxlen]
+    longwordset1 = set(pre_longwordset1)  
 
-    # use a list comprension to get a list of words longer than 10
-    # for word in wordset1
-    # That is:
-    # in a list (e.g. square brackets)
-    # say "[Give me word (for each word in wordset1)
-    #      if len(word) is greater than maxlen]"
-    # then convert the list to a set to we can take the intersection
-    # hint: use set()
-    # name them longwordset1 and longwordset2
+    pre_longwordset2 = [word for word in wordset2 if len(word) > maxlen]
+    longwordset2 = set(pre_longwordset2)  
 
-    longwordset1 = set()  # TODO: fix this line
-    longwordset2 = set()  # TODO: fix this line
-
-    # find the intersection of the two sets
-    # that is, the words in both longwordset1 1 & longwordset2
-    # name this variable longwords
     longwords = longwordset1 & longwordset2
 
     # print the length of the sets and the list
